e671508fe164bc34771698dcdeb15f437d2efefd
DamonHuynh <130857766+DamonHuynh@users.noreply.github.com>
2023-05-17 10:35:48 -0700

    Update OrderList.java

--- OrderList.java
+++ OrderList.java
@@ -3,17 +3,17 @@ package resturant;
 import java.util.ArrayList;
 
 public class OrderList {
-	
+	//initiatilizes arrayList of arrayLists of foods (arrayList of orders)
 	private static ArrayList<ArrayList<Food>> OrderList =new ArrayList<ArrayList<Food>>();
 
 
 
 public static void AddCompletedOrder(ArrayList<Food> f) {
-	
+	//adds order to arrayList
 	OrderList.add(f);
 	
 }
-
+//calculates the total revenue across all orders
 public double TotalRevenue() {
 	
 	double total = 0;
@@ -27,18 +27,18 @@ public double TotalRevenue() {
 	
 	return Math.round(total*100)/100.0;
 }
-
+//returns the total amount of orders 
 public int totalOrders() {
 	
 	return OrderList.size();
 	
 }
-
+//removes an order from the list
 public void removeOrder(int orderNum) {
 	
 	OrderList.remove(orderNum-1);
 }
-
+//prints the total amount of order and the total revenue
 public String toString() {
 	
 	return "The total amount of completed orders for today is: " + totalOrders() + "\nThe total revenue for today is: $"+ TotalRevenue();
